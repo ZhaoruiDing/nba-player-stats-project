338a64dcace20a606124dd03bf0e0109bef36d54
ZhaoruiDing <zhaorui1225@gmail.com>
2018-08-13 20:36:00 +0800

    main skeleton

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Main extends Component{
+  render(){
+    return(
+      <div className="main">
+        <div className="profile">Profile</div>
+        <div className="shotchart">ShotChart</div>
+
+    </div>);
+  }
+}
