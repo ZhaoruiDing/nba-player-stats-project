8defad3a602d424d1106b95631fd17c6ce0063e5
ZhaoruiDing <zhaorui1225@gmail.com>
2018-08-13 23:05:17 +0800

    nba api call

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import {Profile} from "./Profile"
-
+import {Profile} from "./Profile";
+import nba from 'nba';
 export class Main extends Component{
+  state = {
+    playerId: nba.findPlayer("Chris Paul").playerId,
+    playerInfo: {}
+  }
+  componentWillMount(){
+    nba.stats.playerInfo({PlayerID: this.state.playerId}).then((info) => {
+      console.log(info);
+      const playerInfo = Object.assign({},info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+      console.log(playerInfo);
+      this.setState({
+        playerInfo: playerInfo
+      });
+    });
+  }
   render(){
     return(
       <div className="main">
-        <Profile/>
+        <Profile playerInfo={this.state.playerInfo}/>
         <div className="shotchart">ShotChart</div>
 
     </div>);
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,15 +2,54 @@ import React, {Component} from 'react'
 
 export class Profile extends Component{
   render(){
+    const {
+      playerName,
+      playerId,
+      teamAbbreviation,
+      teamCity,
+      teamName,
+      height,
+      weight,
+      pts,
+      reb,
+      ast,
+      pie
+    } = this.props.playerInfo;
+
+
     return (
       <div className="profile">
-        <div className="player-name">James Harden</div>
-        <img className="player-pic" src="https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/201935.png"/>
+        <div className="player-name">{`${playerName}`}</div>
+        <img className="player-pic" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${playerId}.png`}/>
         <div className="profile-entry">
           <div className="profile-entry-left">Team</div>
-          <div className="profile-entry-right">Houston Rockets</div>
+          <div className="profile-entry-right">{`${teamCity} ${teamName}`}</div>
         </div>
-        <img className="team-pic" src="https://stats.nba.com/media/img/teams/logos/HOU_logo.svg"/>
+        <img className="team-pic" src={`https://stats.nba.com/media/img/teams/logos/${teamAbbreviation}_logo.svg`}/>
+        <div className="profile-entry">
+          <div className="profile-entry-left">Height</div>
+          <div className="profile-entry-right">{`${height}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">Weight</div>
+          <div className="profile-entry-right">{`${weight}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">PTS</div>
+          <div className="profile-entry-right">{`${pts}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">REB</div>
+          <div className="profile-entry-right">{`${reb}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">AST</div>
+          <div className="profile-entry-right">{`${ast}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">PIE</div>
+          <div className="profile-entry-right">{`${pie}`}</div>
+        </div>
       </div>
     );
   }
